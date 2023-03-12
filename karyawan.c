46dec1ba7cb348f0fdbf3185fb7dd57625bf9537
fadhil daeng naba <69493052+fdaengnaba@users.noreply.github.com>
2023-03-12 14:53:50 +0700

    add struct node

--- karyawan.c
+++ karyawan.c
@@ -2,3 +2,15 @@
  * Sebuah program database sederhana
  * dengan menggunakan singly linked list
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node {
+    unsigned int id;
+    char name[30];
+    char birthplace[30];
+    char position[50];
+    struct Node *next;
+} node;
